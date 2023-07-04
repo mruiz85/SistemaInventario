38808b50e5345e28d741963702eb87b4f5142274
mruiz85 <garasma2003@gmail.com>
2023-07-04 11:41:25 -0600

    Configuracion del Protecto

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Nombre es Requerido")]
+        [MaxLength(60, ErrorMessage ="Nombre debe ser Maximo de 60 Caracteres") ]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Descripción es Requerido")]
+        [MaxLength(100, ErrorMessage = "Descripcion debe ser maximo de 100 Caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Estado es Requerido")]
+        public bool Estado { get; set; }
+    }
+}
