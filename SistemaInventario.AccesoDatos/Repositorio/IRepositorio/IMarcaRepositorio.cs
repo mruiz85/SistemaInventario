28f8b9ca76d5be7ce7abd5a8bb46f60693cb2f75
mruiz85 <garasma2003@gmail.com>
2023-07-23 21:39:30 -0600

    CRUD de Marca

---
+++ SistemaInventario.AccesoDatos/Repositorio/IRepositorio/IMarcaRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using SistemaInventario.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio.IRepositorio
+{//esta interfaz va a aherde de la generica
+    public interface IMarcaRepositorio: IRepositorio<Marca>
+    {
+        void Actualizar(Marca marca);
+
+    }
+}
